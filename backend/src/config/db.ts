e18865f8c356bf0b1dc6e35e0e6a8bf68d5d5b75
Sushil Kumar <sushilkr820@gmail.com>
2025-12-08 20:19:24 +0530

    chore(backend): remove debug console logs

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -2,8 +2,6 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    console.log("Mongo-env", process.env.MONGO_URI);
-
     const DBConnection = await mongoose.connect(process.env.MONGO_URI!);
     console.log(`MongoDB connected: ${DBConnection?.connection?.host}`);
   } catch (error) {
